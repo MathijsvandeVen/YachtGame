ef5a60174f72a7dd83c639cdf070411bee713efe
Xiang-jong Hu <xiang_jonghu@hotmail.com>
2018-08-30 17:28:45 +0200

    Made SceneSystem and three level scenes with a menu to switch to scenes, go to Home Page and Quit game

--- Assets/Scripts/Systems/ScenesSystem.cs
+++ Assets/Scripts/Systems/ScenesSystem.cs
@@ -1,16 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScenesSystem : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void HomePage()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+    public void LevelOne()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LevelTwo()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LevelThree()
+    {
+        SceneManager.LoadScene(3);
+    }
 }

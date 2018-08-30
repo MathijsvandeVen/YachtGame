3eea4cc43527dd16c363639fa6dedd5700208efd
sangre5 <pjuradosolis@gmail.com>
2018-08-30 16:48:16 +0200

    Flag disappears when player touches the goal

---
+++ Assets/Scripts/Systems/GoalSystem.cs
@@ -0,0 +1,36 @@
+﻿using Unity.Entities;
+using UnityEngine;
+using Unity.Mathematics;
+
+public class GoalSystem : ComponentSystem
+{
+    private struct Data
+    {
+        public ComponentArray<PlayerInputComponent> input;
+    }
+
+    private struct Filter
+    {
+        public GoalComponent gc;
+        public BoxCollider bc;
+    }
+
+    [Inject] private Data _data;
+    protected override void OnUpdate()
+    {
+        var goalEntity = GetEntities<Filter>();
+
+        if (goalEntity.Length > 0)
+        {
+            var distance = 
+                math.distance(goalEntity[0].gc.transform.position.x, _data.input[0].transform.position.x);            
+
+            if(distance < 1)
+            {
+                Object.Destroy(goalEntity[0].gc.gameObject);
+            }
+        }
+
+    }
+
+}

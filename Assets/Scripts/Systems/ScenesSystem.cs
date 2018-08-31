45e6d50199ba7040514528763556510177cb8a23
Xiang-jong Hu <xiang_jonghu@hotmail.com>
2018-08-31 12:52:50 +0200

    Added another method for changing to another scene based on an integer. Added comments

--- Assets/Scripts/Systems/ScenesSystem.cs
+++ Assets/Scripts/Systems/ScenesSystem.cs
@@ -5,34 +5,51 @@ using UnityEngine.SceneManagement;
 
 public class ScenesSystem : MonoBehaviour {
 
+    // Adding scenes to the File >> Build Setting
+    // The scenes get an index number
+
+    // Add to a button the script ScenesSystem.cs
+    // Add to Button (Script) >> On Click () >> Plus (Add to List) >>  Add the player object (button) >> Add to Function add the method from the SceneSystem (replace no function with the method)
+
+    // Method to go to the next level
     public void NextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    // Method to go to the homepage (index = 0)
     public void HomePage()
     {
         SceneManager.LoadScene(0);
     }
 
+    // Method to quit te game
     public void QuitGame()
     {
         Debug.Log("QUIT");
         Application.Quit();
     }
 
-    public void LevelOne()
-    {
-        SceneManager.LoadScene(1);
-    }
+    //// Not necessary anymore
+    //// Method to go to level 1, 2 or 3
+    //public void LevelOne()
+    //{
+    //    SceneManager.LoadScene(1);
+    //}
 
-    public void LevelTwo()
-    {
-        SceneManager.LoadScene(2);
-    }
+    //public void LevelTwo()
+    //{
+    //    SceneManager.LoadScene(2);
+    //}
 
-    public void LevelThree()
+    //public void LevelThree()
+    //{
+    //    SceneManager.LoadScene(3);
+    //}
+
+    // Method to change to another level based on an integer
+    public void ChangeLevel(int LevelNumber)
     {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(LevelNumber);
     }
 }
